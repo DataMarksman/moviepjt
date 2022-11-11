b8df0647080d6442674fff8c7ebf37ef63468226
ganjisriver <ganjisriver18@gmail.com>
2022-11-11 17:21:56 +0900

    complete

--- my-vue-pjt/src/components/WatchListForm.vue
+++ my-vue-pjt/src/components/WatchListForm.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-
+    <div>
+      <h2>보고 싶은 영화</h2>
+    </div>
+    <input type="text" 
+    v-model="inputtedmovie"
+    @keyup.enter="inputMovie"
+    >
+    <button @click="inputMovie">Add</button>
+    
   </div>
 </template>
 
 <script>
 export default {
-  name:'WatchListForm'
+  name:'WatchListForm',
+  data() {
+    return {
+      inputtedmovie : null
+    }
+  },
+  methods: {
+    inputMovie() {
+      this.$store.commit('INPUT_MOVIE', this.inputtedmovie)
+      this.inputtedmovie = null
+      
+    }
+  }
 }
 </script>
 
--- my-vue-pjt/src/views/RandomView.vue
+++ my-vue-pjt/src/views/RandomView.vue
@@ -1,23 +1,36 @@
 <template>
   <div>
-    <div class="container">
-      <div class="d-flex">
-        
-      </div>
+    <div>
+    <button class="btn btn-primary">PICK</button>
     </div>
+    <img :src="getImg" alt="" >
+    <h5>{{movie.title}}</h5>
   </div>
 </template>
 
 <script>
+import _ from 'lodash'
 export default {
   name: 'RandomView',
   data() {
     return {
+      movies: [],
       movie: {}
     }
   },
   methods: {
-    
+    getMovie() {
+      this.movies = this.$store.state.movies
+      this.movie = _.sample(this.movies)
+    }
+  },
+  computed: {
+    getImg() {
+      return `https://image.tmdb.org/t/p/w185${this.movie.poster_path}`
+    }
+  },
+  created() {
+    this.getMovie()
   }
 }
 </script>
