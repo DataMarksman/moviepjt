75424bca6fd3305e3875b28856c0f257437d39b1
ganjisriver <ganjisriver18@gmail.com>
2022-11-11 10:33:08 +0900

    first commit

---
+++ my-vue-pjt/src/views/MovieView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <MovieCard/>
+  </div>
+</template>
+
+<script>
+import MovieCard from '@/components/MoviCard.vue'
+// @ is an alias to /src
+
+
+export default {
+  name: 'MovieView',
+  components: {
+    MovieCard
+  }
+}
+</script>
---
+++ my-vue-pjt/src/views/WatchListView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <WatchListForm/>
+    <WatchListItem/>
+  </div>
+</template>
+
+<script>
+import WatchListForm from '@/components/WatchListForm'
+import WatchListItem from '@/components/WatchListItem'
+export default {
+  name:'WatchListView',
+  components: {
+    WatchListForm,
+    WatchListItem
+  }
+}
+</script>
+
+<style>
+
+</style>
